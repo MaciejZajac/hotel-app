26c131c0628d54f1b6880f1c0c250f3498bee0c0
Maciej Zając <maciej.zajac.197@gmail.com>
2019-08-20 17:22:47 +0200

    finished single room view

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import items from "./data";
 
 const RoomContext = React.createContext();
 
@@ -6,13 +7,48 @@ export default class RoomProvider extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            greeting: "hello",
-            name: "john"
+            rooms: [],
+            sortedRooms: [],
+            featuredRooms: [],
+            loading: true
         };
     }
 
+    // getData
+    componentDidMount() {
+        let rooms = this.formatData(items);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        this.setState({
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
+            loading: false
+        });
+    }
+
+    formatData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id;
+            let images = item.fields.images.map(image => image.fields.file.url);
+            let room = { ...item.fields, images, id };
+
+            return room;
+        });
+        return tempItems;
+    }
+
+    getRoom = slug => {
+        let tempRooms = [...this.state.rooms];
+        const room = tempRooms.find(room => room.slug === slug);
+        return room;
+    };
+
     render() {
-        return <RoomContext.Provider value={{ ...this.state }}>{this.props.children}</RoomContext.Provider>;
+        return (
+            <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+                {this.props.children}
+            </RoomContext.Provider>
+        );
     }
 }
 
