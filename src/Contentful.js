ccbff2c0984c533851d71cca0e714f04b6b6543b
Maciej Zając <maciej.zajac.197@gmail.com>
2019-08-20 23:03:37 +0200

    env development file

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-    space: "n5bvjrg1fgep",
-    accessToken: "gS7MzZ2jR76tvELcDb4Nzrv33qjpzZSPuGOR8RCO7Vw"
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
